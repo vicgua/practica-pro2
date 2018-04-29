ef0282a27ba8884e8bb04ae6df47a562d6273fc0
Victor G <victorg590@gmail.com>
2018-04-29 19:42:51 +0200

    Añadido eco de instrucciones

--- main.cc
+++ main.cc
@@ -25,12 +25,14 @@ int main() {
         if (inst == "poner_prod") {
             string id;
             cin >> id;
+            cout << inst << ' ' << id << endl;
             bool ok = almacen.poner_prod(id);
             if (not ok) cout << "  error" << endl;
 
         } else if (inst == "quitar_prod") {
             string id;
             cin >> id;
+            cout << inst << ' ' << id << endl;
             bool ok = almacen.quitar_prod(id);
             if (not ok) cout << "  error" << endl;
 
@@ -39,6 +41,8 @@ int main() {
             string id;
             int cantidad;
             cin >> sala >> id >> cantidad;
+            cout << inst << ' ' << sala << ' ' << id << ' ' << cantidad;
+            cout << endl;
             if (almacen.existe_producto(id)) {
                 int sobran = almacen.poner_items(sala, id, cantidad);
                 cout << "  " << sobran << endl;
@@ -50,6 +54,7 @@ int main() {
             string id;
             int cantidad;
             cin >> sala >> id >> cantidad;
+            cout << inst << ' ' << sala << ' ' << id << ' ' << cantidad << endl;
             if (almacen.existe_producto(id)) {
                 int faltan = almacen.quitar_items(sala, id, cantidad);
                 cout << "  " << faltan << endl;
@@ -60,6 +65,7 @@ int main() {
             string id;
             int cantidad;
             cin >> id >> cantidad;
+            cout << inst << ' ' << id << ' ' << cantidad << endl;
             int sobran = almacen.distribuir(id, cantidad);
             if (sobran == -1)
                 cout << "  error" << endl;
@@ -69,21 +75,25 @@ int main() {
         } else if (inst == "compactar") {
             int sala;
             cin >> sala;
+            cout << inst << ' ' << sala << endl;
             almacen.compactar(sala);
 
         } else if (inst == "reorganizar") {
             int sala;
             cin >> sala;
+            cout << inst << ' ' << sala << endl;
             almacen.reorganizar(sala);
 
         } else if (inst == "redimensionar") {
             int sala;
             int f, c;
             cin >> sala >> f >> c;
+            cout << inst << ' ' << sala << ' ' << f << ' ' << c << endl;
             bool ok = almacen.redimensionar(sala, f, c);
             if (not ok) cout << "  error" << endl;
 
         } else if (inst == "inventario") {
+            cout << inst << endl;
             const map<string, int> &inventario = almacen.inventario();
             map<string, int>::const_iterator it;
             for (it = inventario.begin(); it != inventario.end(); ++it) {
@@ -95,18 +105,21 @@ int main() {
         } else if (inst == "escribir") {
             int sala;
             cin >> sala;
+            cout << inst << ' ' << sala << endl;
             almacen.escribir(sala, cout);
 
         } else if (inst == "consultar_pos") {
             int sala;
             int f, c;
             cin >> sala >> f >> c;
+            cout << inst << ' ' << sala << f << ' ' << c << endl;
             string id = almacen.consultar_pos(sala, f, c);
             cout << "  " << id << endl;
 
         } else if (inst == "consultar_prod") {
             string id;
             cin >> id;
+            cout << inst << ' ' << id << endl;
             int num = almacen.consultar_prod(id);
             if (num == -1)
                 cout << "  error" << endl;
