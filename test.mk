390460ebee48024b6cc975227b132507e68b857e
Victor G <victorg590@gmail.com>
2018-05-18 13:09:36 +0200

    Actualitzados tests de pruebas (con TODOs)

--- test.mk
+++ test.mk
@@ -1,6 +1,4 @@
 # Makefile for tests
-PYTHON3 = python3
-
 .PHONY: all public-tests custom-tests
 
 all: public-tests custom-tests
@@ -12,4 +10,4 @@ custom-tests: program.exe custom.inp custom.cor
 	./program.exe < custom.inp | diff - custom.cor
 
 custom.inp custom.cor: testpp.py custom_tests/*
-	$(PYTHON3) testpp.py -o custom custom_tests
+	./testpp.py -o custom custom_tests
